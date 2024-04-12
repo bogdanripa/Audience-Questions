d137658597aca71f8e1d0f6e48c5b77dab87443f
Bogdan Ripa <bogdanripa@gmail.com>
2024-04-12 11:16:07 +0300

    Updated icons path

--- client/src/Icons.tsx
+++ client/src/Icons.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <div className="logos">
-            <img src="/public/heart.svg" width="30"/>
-            <a href="https://genezio.com/"><img src="/public/genezio.svg" width="30"/></a>
-            <a href="https://github.com/bogdanripa/Audience-Questions"><img src="/public/github.svg" width="30"/></a>
+            <img src="/heart.svg" width="30"/>
+            <a href="https://genezio.com/"><img src="/genezio.svg" width="30"/></a>
+            <a href="https://github.com/bogdanripa/Audience-Questions"><img src="/github.svg" width="30"/></a>
         </div>
     );
 }
